af01e2e352003e56b9c0948449e80cb7ebddace7
Eric Egan <egan1551@gmail.com>
2017-05-11 17:12:38 +0200

    --amend

--- config/initializers/database.js
+++ config/initializers/database.js
@@ -1,7 +1,6 @@
 import Config from '../environment';
 import db_config from '../../knexfile.js';
 import winston from 'winston';
-import async from 'async';
 
 var env = Config.get('NODE:ENV');
 var knex = require('knex')(db_config[env]);
