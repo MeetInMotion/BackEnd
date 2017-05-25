0c8c336e2d8d8094cb78ce5081753e772a954a7f
Eric Egan <egan1551@gmail.com>
2017-05-25 11:37:43 +0200

    removed nconf and started on event path

--- api/routes/events.js
+++ api/routes/events.js
@@ -1,8 +1,19 @@
-//import Event from '../models/event';
+import Event from '../models/event';
 
 module.exports = function(router){
   router.route('/')
-    .post((_req, _res) =>{
-      
+    .post((req, res) =>{
+      var event = {};
+      event.title = "title1";
+      event.date = "2017-06-03";
+      event.time = "18:00+02";
+      event.location_id = req.body.location_id;
+      new Event(event).save().then(function(model){
+        console.log(model);
+        res.json(model);
+      }).catch(function(err){
+        console.log("here is an error");
+        console.log(err);
+      });
     });
 };
